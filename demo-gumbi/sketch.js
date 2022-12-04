2e43716123218ba2339f92cd7961138da13c7496
amadeusjosip <85517142+amadeusjosip@users.noreply.github.com>
2022-12-04 16:19:01 +0100

    Update sketch.js

--- demo-gumbi/sketch.js
+++ demo-gumbi/sketch.js
@@ -1,8 +1,8 @@
 var font;var font2;var font1;
 function preload(){
-  font = loadFont("fontovi/Aeonik-Medium.ttf"); 
-  font1 = loadFont("fontovi/BauhausRegular.ttf");
-  font2 = loadFont("fontovi/Aeonik-Medium.ttf");  
+  font = loadFont("../assets/fontovi/font1.ttf"); 
+  font1 = loadFont("../assets/fontovi/font2.ttf");
+  font2 = loadFont("../assets/fontovi/font1.ttf");  
 
 }
 
