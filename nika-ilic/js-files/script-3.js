9941000855818e88078cefa9fe1e705e9f45f991
amadeusjosip <85517142+amadeusjosip@users.noreply.github.com>
2023-03-27 15:38:48 +0200

    Add files via upload

---
+++ nika-ilic/js-files/script-3.js
@@ -0,0 +1,232 @@
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-1');
+        }
+        else{
+            entry.target.classList.remove('scroll-1');
+        }
+ 
+    });
+}); 
+
+const hiddenElements1 = document.querySelectorAll('.item-1');
+hiddenElements1.forEach((el) => observer1.observe(el));
+///////////////////////////
+
+
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-2i3');
+        }
+        else{
+            entry.target.classList.remove('scroll-2i3');
+        }
+ 
+    });
+}); 
+
+const hiddenElements2 = document.querySelectorAll('.item-2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+///////////////////////////
+
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-2i3');
+        }
+        else{
+            entry.target.classList.remove('scroll-2i3');
+        }
+ 
+    });
+}); 
+
+const hiddenElements3 = document.querySelectorAll('.item-3');
+hiddenElements3.forEach((el) => observer3.observe(el));
+///////////////////////////
+
+
+const observer0 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-0');
+        }
+        else{
+            entry.target.classList.remove('scroll-0');
+        }
+ 
+    });
+}); 
+
+const hiddenElements0 = document.querySelectorAll('.item-0');
+hiddenElements0.forEach((el) => observer0.observe(el));
+///////////////////////////
+
+const observerx = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x');
+        }
+        else{
+            entry.target.classList.remove('scroll-x');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx = document.querySelectorAll('.item-x');
+hiddenElementsx.forEach((el) => observerx.observe(el));
+///////////////////////////
+///////////////////////////
+
+const observerx2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x2');
+        }
+        else{
+            entry.target.classList.remove('scroll-x2');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx2 = document.querySelectorAll('.item-x2');
+hiddenElementsx2.forEach((el) => observerx2.observe(el));
+///////////////////////////
+///////////////////////////
+
+const observerx3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x3');
+        }
+        else{
+            entry.target.classList.remove('scroll-x3');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx3 = document.querySelectorAll('.item-x3');
+hiddenElementsx3.forEach((el) => observerx3.observe(el));
+//////////////////////////////////////////////////////
+
+const observerx4 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x4');
+        }
+        else{
+            entry.target.classList.remove('scroll-x4');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx4 = document.querySelectorAll('.item-x4');
+hiddenElementsx4.forEach((el) => observerx4.observe(el));
+///////////////////////////
+//////////////////////////////////////////////////////
+
+const observerx5 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x5');
+        }
+        else{
+            entry.target.classList.remove('scroll-x5');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx5 = document.querySelectorAll('.item-x5');
+hiddenElementsx5.forEach((el) => observerx5.observe(el));
+/////////////////////////////////////////////////////////////////////////////////
+
+const observerx6 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x6');
+        }
+        else{
+            entry.target.classList.remove('scroll-x6');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx6 = document.querySelectorAll('.item-x6');
+hiddenElementsx6.forEach((el) => observerx6.observe(el));
+///////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+const observerx7 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x7');
+        }
+        else{
+            entry.target.classList.remove('scroll-x7');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx7 = document.querySelectorAll('.item-x7');
+hiddenElementsx7.forEach((el) => observerx7.observe(el));
+///////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+const observerx0 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x0');
+        }
+        else{
+            entry.target.classList.remove('scroll-x0');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx0 = document.querySelectorAll('.item-x0');
+hiddenElementsx0.forEach((el) => observerx0.observe(el));
+///////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+const observerx8 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if(entry.isIntersecting){
+            entry.target.classList.add('scroll-x8');
+        }
+        else{
+            entry.target.classList.remove('scroll-x8');
+        }
+ 
+    });
+}); 
+
+const hiddenElementsx8 = document.querySelectorAll('.item-x8');
+hiddenElementsx8.forEach((el) => observerx8.observe(el));
+///////////////////////////
