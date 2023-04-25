2b690ecc85c2a10df73d3dde5f1816ebd28ee7c2
amadeusjosip <85517142+amadeusjosip@users.noreply.github.com>
2023-04-25 15:48:31 +0200

    Update script.js

--- myportafolio/script.js
+++ myportafolio/script.js
@@ -1,5 +1,9 @@
 window.onbeforeunload = function() {
-    window.scrollTo(0, 0);
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
 };
 
 var avatar = document.getElementById("avatar");   
