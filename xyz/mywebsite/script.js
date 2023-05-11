5c5bd7ea29b767c2255b7861cf2fa8dd4d5d1a7a
AmadeusJosip <josip.amadeus@gmail.com>
2023-05-11 15:28:25 +0200

    mywebsite/ modified 2

--- xyz/mywebsite/script.js
+++ xyz/mywebsite/script.js
@@ -1,20 +1,37 @@
 const original = document.querySelector("#original");
 const secret = document.querySelector("#secret");
 
-const bodi = document.querySelector("body");
+const gumb = document.querySelector("body");
+ 
 
-secret.style.display = "none";
-original.style.display = "flex";
+original.style.clipPath = "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)";
+original.style.transform = "translateY(0px)"; 
+original.style.opacity = "1";
+original.style.transition = "1.38s cubic-bezier(0.76, 0, 0.24, 1)";
+
+secret.style.transform = "translateY(30px)";
+secret.style.transition = "1.38s cubic-bezier(0.76, 0, 0.24, 1)";
+
 
 var br = 0;
-bodi.onclick = function(){
+gumb.onclick = function(){
     if(br % 2 == 0){       
-        original.style.display = "none";
-        secret.style.display = "flex";
+        original.style.clipPath = "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)"; 
+        original.style.transform = "translateY(-20px)";
+        original.style.opacity = "0";
+        original.style.transition = "1.38s cubic-bezier(0.76, 0, 0.24, 1)";
+
+        secret.style.transform = "translateY(0px)";
+        secret.style.transition = "1.38s cubic-bezier(0.76, 0, 0.24, 1)";
     }
     else{
-        original.style.display = "flex";
-        secret.style.display = "none";
+        original.style.clipPath = "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)";
+        original.style.transform = "translateY(0px)"; 
+        original.style.opacity = "1";
+        original.style.transition = "1.38s cubic-bezier(0.76, 0, 0.24, 1)";
+
+        secret.style.transform = "translateY(30px)";
+        secret.style.transition = "1.38s cubic-bezier(0.76, 0, 0.24, 1)";
     }
     br++;
 }
